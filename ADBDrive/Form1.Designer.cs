64ce5cfc513a3f849e9da1419b02eeaaea1b427b
lrst6963 <lrsts@qq.com>
2024-08-22 09:28:29 +0800

    update

--- ADBDrive/Form1.Designer.cs
+++ ADBDrive/Form1.Designer.cs
@@ -28,16 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(65, 54);
+            this.button1.Location = new System.Drawing.Point(128, 55);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 54);
+            this.button1.Size = new System.Drawing.Size(176, 68);
             this.button1.TabIndex = 0;
             this.button1.Text = "下载";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,28 +58,127 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button2.Location = new System.Drawing.Point(216, 54);
+            this.button2.Location = new System.Drawing.Point(332, 55);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(132, 54);
+            this.button2.Size = new System.Drawing.Size(176, 68);
             this.button2.TabIndex = 1;
             this.button2.Text = "安装";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(41, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "驱动:";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(128, 149);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 68);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "下载";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(332, 149);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(176, 68);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "安装";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(41, 165);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 35);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "工具:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.菜单ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(553, 28);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 菜单ToolStripMenuItem
+            // 
+            this.菜单ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripSeparator1});
+            this.菜单ToolStripMenuItem.Name = "菜单ToolStripMenuItem";
+            this.菜单ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.菜单ToolStripMenuItem.Text = "文件";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(122, 26);
+            this.toolStripMenuItem1.Text = "退出";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(119, 6);
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(0, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(560, 2);
+            this.label3.TabIndex = 7;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(415, 165);
+            this.ClientSize = new System.Drawing.Size(553, 239);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "ADB驱动安装";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,6 +186,15 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 菜单ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ADBDrive/Form1.cs
+++ ADBDrive/Form1.cs
@@ -7,6 +7,7 @@ using System.Security.Cryptography;
 using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ADBDrive
 {
@@ -19,13 +20,14 @@ namespace ADBDrive
 
         readonly string TEMP_DIR = Environment.GetEnvironmentVariable("TEMP");
         string url = "https://dl.google.com/android/repository/usb_driver_r13-windows.zip";
-        string tools_url = "https://dl.google.com/android/repository/platform-tools-latest-windows.zip";
-        string md5 = "1264c572565b136c4a5b5ef75a7a1f47";
+        string tools_url = "https://dl.google.com/android/repository/platform-tools_r35.0.0-windows.zip";
+        string md5_driver = "1264c572565b136c4a5b5ef75a7a1f47";
+        string md5_tools = "e8a786dc533d91b1e6c3dc38722bbaba";
 
         private void Button1_Click(object sender, EventArgs e)
         {
             string zip_file = TEMP_DIR + "\\usb_driver_r13-windows.zip";
-            if (Directory.Exists(TEMP_DIR + "\\usb_driver") && File.Exists(zip_file) && CalMD5(zip_file) == "1264c572565b136c4a5b5ef75a7a1f47")
+            if (Directory.Exists(TEMP_DIR + "\\usb_driver") && File.Exists(zip_file) && CalMD5(zip_file) == md5_driver)
             {
                 MessageBox.Show("已下载，切勿重新下载！");
             }
@@ -33,7 +35,7 @@ namespace ADBDrive
             {
                 var t = Task.Run(() => D(url, zip_file));
                 t.Wait();
-                if (CalMD5(zip_file) == md5)
+                if (CalMD5(zip_file) == md5_driver)
                 {
                     MessageBox.Show("下载完成！");
                 }
@@ -70,6 +72,30 @@ namespace ADBDrive
             }
         }
 
+        public static void 复制目录下所有文件到另外一个路径(string sourceDirectory, string destinationDirectory)
+        {
+            try
+            {
+                // 获取源目录中的所有文件
+                string[] files = Directory.GetFiles(sourceDirectory);
+
+                // 遍历所有文件并移动它们到目标目录
+                foreach (string file in files)
+                {
+                    string fileName = Path.GetFileName(file);
+                    string destinationFile = Path.Combine(destinationDirectory, fileName);
+                    File.Copy(file, destinationFile);
+                    Console.WriteLine("文件移动成功: " + fileName);
+                }
+
+                Console.WriteLine("所有文件移动完成");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("文件移动失败: " + e.Message);
+            }
+        }
+
         public static int Execute(bool NoWindow, string command)
         {
             Process process = new Process();
@@ -96,7 +122,6 @@ namespace ADBDrive
         {
             WindowsIdentity current = WindowsIdentity.GetCurrent();
             WindowsPrincipal windowsPrincipal = new WindowsPrincipal(current);
-            //WindowsBuiltInRole可以枚举出很多权限，例如系统用户、User、Guest等等   
             return windowsPrincipal.IsInRole(WindowsBuiltInRole.Administrator);
         }
 
@@ -106,8 +131,70 @@ namespace ADBDrive
             {
                 this.Text = Text + "（管理员运行）";
             }
+            else
+            {
+                this.Text = Text + "（非管理员运行）";
+            }
             ShowIcon = false;
             //MessageBox.Show(Environment.GetEnvironmentVariable("PATH"));
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string zip_file = TEMP_DIR + "\\platform-tools_r35.0.0-windows.zip";
+            if (Directory.Exists(TEMP_DIR + "\\platform-tools") && File.Exists(zip_file) && CalMD5(zip_file) == md5_tools)
+            {
+                MessageBox.Show("已下载，切勿重新下载！");
+            }
+            else
+            {
+                var t = Task.Run(() => D(tools_url, zip_file));
+                t.Wait();
+                if (CalMD5(zip_file) == md5_tools)
+                {
+                    MessageBox.Show("下载完成！");
+                }
+                else
+                {
+                    MessageBox.Show("下载失败！");
+                }
+                if (Directory.Exists(TEMP_DIR + "\\platform-tools"))
+                {
+                    Directory.Delete(TEMP_DIR + "\\platform-tools", true);
+                }
+                ZipFile.ExtractToDirectory(zip_file, TEMP_DIR);
+                
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string zip_file = TEMP_DIR + "\\platform-tools_r35.0.0-windows.zip";
+            if (Directory.Exists(TEMP_DIR + "\\platform-tools") && File.Exists(zip_file) && CalMD5(zip_file) == md5_tools)
+            {
+                string infp = TEMP_DIR + "\\platform-tools";
+                复制目录下所有文件到另外一个路径(infp, "C:\\Windows");
+                //string arguments = "adb.exe";
+                MessageBox.Show("工具安装成功！");
+            }
+            else
+            {
+                MessageBox.Show("未下载");
+            }
+
+/*            if (Execute(true, arguments) == 0)
+            {
+                MessageBox.Show("工具安装成功！");
+            }
+            else
+            {
+                MessageBox.Show("工具安装失败！");
+            }*/
         }
     }
 }
